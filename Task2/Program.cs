f3eb1cfc4b571ec0fcf5ea9b937a5f841547b866
Зейтын Бахытбек Кылышбекович <@corp.itstep>
2017-12-13 16:43:30 +0600

    first

---
+++ Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+
+            for(int i=0; i<numbers.Length; i++)
+            {
+                numbers[i] = i;
+            }
+
+            try
+            {
+              Console.WriteLine(numbers[6]);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Завершение обработки массива");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Домашнее задание 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Домашнее_задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Method();
+
+            Console.ReadLine();
+
+        }
+
+        static void Method()
+        {
+            double? a = null, b = 0;
+            try
+            {
+                if (a == null ||b == null)
+                {
+                    throw new ArgumentNullException();
+                    
+                }
+                else if (b == 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("Результат равен = " + a / b);
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+;
+        }
+
+        
+    }
+
+}
